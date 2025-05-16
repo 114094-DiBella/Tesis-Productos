4874895124a5b57e89524fc61c0861bf735938bf
agustin <114094@tecnicatura.frc.utn.edu.ar>
2025-05-16 19:00:26 -0300

    act en el data.sql

--- productservices/src/main/resources/data.sql
+++ productservices/src/main/resources/data.sql
@@ -14,7 +14,7 @@ INSERT INTO marcas (name) VALUES ('Under Armour');
 -- Inserts para la tabla products (usando UUID generado)
 INSERT INTO products (id, name, marca_id, category_id, size, color, price, stock, active, created_at, updated_at)
 VALUES
-    ('11111111-1111-1111-1111-111111111111', 'Camiseta Deportiva', 1, 1, 'S', 'Negro', 29.99, 100, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+    ('11111111-1111-1111-1111-111111111111', 'Camiseta Deportiva', 1, 1, 'S', 'Negro', 100.10, 100, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 -- Inserts para la tabla product_images
 INSERT INTO product_images (image_url, product_id, main, display_order)
 VALUES
